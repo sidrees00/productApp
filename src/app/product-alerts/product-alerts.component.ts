784d2863ed319f23f220b8dca8215431d2ae274c
Usman <sidrees88@hotmail.com>
2019-07-06 19:11:28 -0400

    output

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { products } from '../products';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -8,6 +7,7 @@ import { products } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter;
   
   constructor() { }
 
@@ -23,5 +23,7 @@ Notes:
 1) A decorator is placed directly before the class it affects.  E.g. - The @Component decorator is placed direclty before the ProductAlertsComponent class.  The decorator provides metadata about the component, including its templates, styles, and a selector.
 
 2) The selector is the name you give the Angular component when it is rendered as an HTML element on the page. By convention, Angular component selectors begin with the prefix app-, followed by the component name.
+
+3) The @Output decorator and event emitter instance makes it possible for the product alert component to emit an event when the value of the notify property changes.
 
 */
