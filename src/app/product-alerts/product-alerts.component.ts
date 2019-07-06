31c7ce8f6845fbfc716875f0833fef28175bbd14
Usman <sidrees88@hotmail.com>
2019-07-06 19:03:52 -0400

    input

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-
+  @Input() product;
+  
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+/*
+Notes: 
+
+1) A decorator is placed directly before the class it affects.  E.g. - The @Component decorator is placed direclty before the ProductAlertsComponent class.  The decorator provides metadata about the component, including its templates, styles, and a selector.
+
+2) The selector is the name you give the Angular component when it is rendered as an HTML element on the page. By convention, Angular component selectors begin with the prefix app-, followed by the component name.
+
+*/
