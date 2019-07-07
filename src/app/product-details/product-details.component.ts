98aece87e839fcabc28457a1144ba8600458a7fc
Usman <sidrees88@hotmail.com>
2019-07-07 19:04:48 -0400

    Add route, routerLink and dynamically route details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[params.get('productId')]
+    })
   }
 
 }
+
+/*
+Notes:
+1) The ActivatedRoute is specific to each routed component loaded by the Angular Router. It contains information about the route, its parameters, and additional data associated with the route.
+
+*/
