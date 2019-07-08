49ae355a3d2e54d94189811ae692c0dfc2b30647
Usman <sidrees88@hotmail.com>
2019-07-07 22:10:40 -0400

    Use cart service to retrieve shipping costs via HTTP

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService,
+  ) {
+    this.shippingCosts = cartService.getShippingPrices();
+   }
 
   ngOnInit() {
   }
 
 }
+
+/*
+Notes:
+
+1) Data is currently routed as follows: ShippingComponent --> cartService --> HttpClient --> /shipping.json
+
+*/
+
