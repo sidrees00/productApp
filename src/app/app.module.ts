62dea64e0ddd8e8c1f74cb651e4a2a09677eb27b
Usman <sidrees88@hotmail.com>
2019-07-07 21:59:10 -0400

    Implement HttpClient and use it to GET data from local file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -14,6 +16,7 @@ import { CartComponent } from './cart/cart.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
@@ -31,3 +34,15 @@ import { CartComponent } from './cart/cart.component';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+/*
+Notes:
+
+1) This file contains imports and functionality that is available to the entire app.
+
+2) Adding HttpClientModule to the imports array here registers Angular's HttpClient providers globally.
+
+3) Alternatively, the HttpClient service is what you inject into your services to fetch data and interact with external APIs and resources.
+
+
+*/
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +7,9 @@ import { Injectable } from '@angular/core';
 export class CartService {
   items = [];
   
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   addToCart(product) {
     this.items.push(product);
@@ -20,11 +23,24 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json')
+  }
 }
 
 /*
 Notes: 
-1) Services are an integral part of Angular applications. In Angular, a service is an instance of a class that can be made available to any part of your application using Angular's dependency injection system.
 
-Services are the place where you share data between parts of your application. For the online store, the cart service is where you store your cart data and methods.
+1) In Angular, a service is an instance of a class that can be made available to any part of your application using Angular's dependency injection system.
+
+2) Services are the place where you share data between parts of your application. For this app, the cart service is where you store your cart data and methods.
+
+3) Multiple component can leverage the same service.
+
+3) HttpClient is a built-in way to fetch data from external APIs and provide them as a stream.
+
+4) The HttpClient service is what you inject into your services to fetch data and interact with external APIs and resources.
+
+5) The getShippingPrices() method will return data from within a local file.
 */
